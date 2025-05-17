dc9817e3cdba48043af18197886bd26b2f4f57ef
fkgrkyr <fkgrkyr@gmail.com>
2025-05-17 16:05:57 +0900

    feat: 프로젝트 기본 구조 및 초기 레이아웃 파일 설정

---
+++ ShoppingMall/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import SignIn from './pages/SignIn';
+import Navbar from './components/Navbar';
+import './index.css';
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ ShoppingMall/src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+const SingIn = () => {
+    return(
+        <div>
+            <h1>SingIn</h1>
+        </div>
+    )
+}
+
+export default SingIn;
