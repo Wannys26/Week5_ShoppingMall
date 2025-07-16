3e8a20a439479a56ff8506054bd0a165fb82ac72
fkgrkyr <fkgrkyr@gmail.com>
2025-07-16 22:23:11 +0900

    fix: console.log 제거

--- ShoppingMall/src/components/Card.jsx
+++ ShoppingMall/src/components/Card.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import defaultImage from '../assets/defaultImage.png';
 
 export default function Card({ id, category, name, price, onAddToCart }) {
-  console.log('🟣 Card props:', { id, category, name, price });
+  console.log('Card props:', { id, category, name, price });
   
   const navigate = useNavigate();
 
