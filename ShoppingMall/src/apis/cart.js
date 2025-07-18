b9863d4d1f14b13211685536f2d8f933b7eb081f
KimChaewon215 <kimchaewon215@gmail.com>
2025-07-18 19:52:21 +0900

    cart.js 오류 수정

--- ShoppingMall/src/apis/cart.js
+++ ShoppingMall/src/apis/cart.js
@@ -1,22 +1,26 @@
 import instance from './instance';
+const userId = 2;
 
-// 장바구니 조회
+// 1) 장바구니 조회
 export const getCart = async () => {
-  const res = await instance.get('/cart');
-  return res.data;
+  const res = await instance.get(`/cart/items/${userId}`);
+  return res.data; 
 };
-
-// 장바구니 아이템 추가
+// 2) 장바구니 아이템 추가
 export const addToCart = async (productId, quantity) => {
-  const res = await instance.post('/cart', {
-    productId,
-    quantity,
-  });
-  return res.data;
+  const res = await instance.post(
+    `/cart/items/${userId}`,
+    { productId, quantity },     
+    { withCredentials: true }  
+  );
+  return res.data; 
 };
 
-// 장바구니 아이템 삭제
-export const deleteCartItem = async (id) => {
-  const res = await instance.delete(`/cart/${id}`);
+// 3) 장바구니 아이템 삭제
+export const deleteCartItem = async (cartItemId) => {
+  const res = await instance.delete(
+     `/cart/items/${cartItemId}?userId=${userId}`,
+    { withCredentials: true }
+  );
   return res.data;
 };
